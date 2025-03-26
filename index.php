189b79bc659c26d4744ce9eb3c5e4dc3d32786e3
sept-oga <178131979+sept-oga@users.noreply.github.com>
2025-03-26 13:02:34 +0000

    PHPⅡ HTMLとPHP(2)まで

--- index.php
+++ index.php
@@ -12,10 +12,32 @@
     }
   }
 
+  $$juice = new Menu('JUICE');
+  $coffee = new Menu('COFFEE');
   $curry = new Menu('CURRY');
   $pasta = new Menu('PASTA');
 
+  $menus = array($juice, $coffee, $curry, $pasta);
+
 ?>
 
-<p><?php echo $curry -> name ?></p>
-<p><?php echo $pasta -> name ?></p>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      
+      <?php foreach ($menus as $menu): ?>
+        <h3><?php echo $menu -> name ?></h3>
+      <?php endforeach ?>
+    </div>
+  </div>
+</body>
+</html>
