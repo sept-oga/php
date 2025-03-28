79f08e8045359b01b364bf841af12046da899212
sept-oga <178131979+sept-oga@users.noreply.github.com>
2025-03-28 11:55:19 +0000

    PHPⅡ ファイルを分けようまで

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<?php
-  // クラスの定義は大文字
-  class Menu{
-    public $name;
-
-    public function __construct($name) {
-      $this->name = $name;
-    }
-
-    public function hello() {
-      echo '私は'.$this->name.'です';
-    }
-  }
-
-  $$juice = new Menu('JUICE');
-  $coffee = new Menu('COFFEE');
-  $curry = new Menu('CURRY');
-  $pasta = new Menu('PASTA');
-
-  $menus = array($juice, $coffee, $curry, $pasta);
-
+<?php 
+  require_once('data.php');
 ?>
 
 <!DOCTYPE html>
@@ -33,10 +14,10 @@
   <div class="menu-wrapper container">
     <h1 class="logo">Café Progate</h1>
     <div class="menu-items">
-      
       <?php foreach ($menus as $menu): ?>
-        <h3><?php echo $menu -> name ?></h3>
+        <h3 class="menu-item-name"><?php echo $menu->name ?></h3>
       <?php endforeach ?>
+      
     </div>
   </div>
 </body>
